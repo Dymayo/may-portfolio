dccef28088e30bec21fb9feec7bf2a87a88cae82
Dymayo <bouhlelmaram05@gmail.com>
2025-06-25 01:54:35 +0100

    feat: Integrate Spline models and enhance ShowcaseSection with animations

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,28 +1,20 @@
-import React from "react";
-import Cubes from "../components/Cubes";
-import { words } from "../constants";
-import Button from "../components/button";
-import HeroExperience from "../components/HeroModels/HeroExperience";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+
+import Button from "../components/Button";
+import { words } from "../constants";
+
+import HeroEperienceSuper from "../components/models/hero_models/HeroEperienceSuper";
 
 const Hero = () => {
   useGSAP(() => {
     gsap.fromTo(
       ".hero-text h1",
-      {
-        y: 50,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        stagger: 0.2,
-        duration: 1,
-        ease: "power2.inOut",
-      }
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
     );
   });
+
   return (
     <section id="hero" className="relative overflow-hidden">
       <div className="absolute top-0 left-0 z-10">
@@ -32,16 +24,16 @@ const Hero = () => {
       <div className="hero-layout">
         {/* LEFT: Hero Content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-7">
             <div className="hero-text">
               <h1>
-                I like
+                Shaping
                 <span className="slide">
                   <span className="wrapper">
                     {words.map((word, index) => (
                       <span
                         key={index}
-                        className="flex items-center md:gap-3 gap-1 pb-2 "
+                        className="flex items-center md:gap-3 gap-1 pb-2"
                       >
                         <img
                           src={word.imgPath}
@@ -54,26 +46,27 @@ const Hero = () => {
                   </span>
                 </span>
               </h1>
-              <h1>Visions and Ideas</h1>
-              <h1>into Clean Experiences</h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
             </div>
+
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I'm May, a UI/UX designer doubling as a frontend develolper
-              <br />
-              bridging beauty and function.
+              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              code.
             </p>
+
             <Button
-              className="md:w-80 md:h-16 w-60 h-12"
-              id="button"
               text="See My Work"
+              className="md:w-80 md:h-16 w-60 h-12"
+              id="counter"
             />
           </div>
         </header>
 
-        {/* RIGHT: 3D Model */}
+        {/* RIGHT: 3D Model or Visual */}
         <figure>
           <div className="hero-3d-layout">
-            <HeroExperience />
+            <HeroEperienceSuper />
           </div>
         </figure>
       </div>
